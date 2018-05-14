df52cf3a8d74d20a16e22fc1481501689595925d
aevitas <m4rtijn@gmail.com>
2018-05-14 12:05:47 +0200

    Add express and base service code

---
+++ src/service.ts
@@ -0,0 +1,30 @@
+import * as express from "express";
+import * as logger from "morgan";
+import * as bodyParser from "body-parser";
+
+export class Service {
+
+    public express: express.Application;
+
+    public constructor() {
+        this.express = express();
+        this.configureMiddleware();
+        this.configureRoutes();
+    }
+
+    private configureMiddleware(): void {
+        this.express.use(logger("development"));
+        this.express.use(bodyParser.json());
+        this.express.use(bodyParser.urlencoded({ extended: false }));
+    }
+
+    private configureRoutes(): void {
+        const router = express.Router();
+
+        // Every service should have a health endpoint to ensure we can check the service's health
+        // using external montoring tools.
+        router.get("/health", (_request, response, _next) => {
+            response.sendStatus(200);
+        });
+    }
+}
